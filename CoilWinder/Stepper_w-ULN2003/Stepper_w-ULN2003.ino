d3d3aaa5397bd8c2e23228e2c456cced078112b6
Mr. Q <qbytes-dq@users.noreply.github.com>
2021-02-10 21:20:00 -0500

    Update Stepper_w-ULN2003.ino
    Added initial logic for calculations

--- CoilWinder/Stepper_w-ULN2003/Stepper_w-ULN2003.ino
+++ CoilWinder/Stepper_w-ULN2003/Stepper_w-ULN2003.ino
@@ -9,7 +9,7 @@
   // ------------------------------------------------------
     const byte motorEnable =  A0;
     const byte motor1A =  A1;
-    const byte motor2a =  A2;
+    const byte motor2A =  A2;
 
   // ------------------------------------------------------
   // initialize Stepper motor.
@@ -22,21 +22,22 @@
   */
   
   // 28BYJ-48 Gear Reduction Ratio
-  double gearRatios = (32.0/9.0) * (22.0/11.0) * (26.0/9.0) * (31.0/10.0) *(28.0/22.0)*(22.0/20.0); // gears on top *(28.0/22.0)*(22.0/20.0);
+  double topRatios = (28.0/22.0)* (22.0/22.0) *(22.0/20.0); // gears on top *(28.0/22.0)*(22.0/20.0);
+  double gearRatios = (32.0/9.0) * (22.0/11.0) * (26.0/9.0) * (31.0/10.0) * topRatios;
   
   // Stride Angle ： 5.625° x 1/64 datasheet is wrong
   // Stride Angle ： 11.25° x ~1/64 website above is correct
 
   // Cir
-  #define PI 3.1415926535897932384626433832795
+  #define PI 3.1415926535897932384626433832795  // Overkill.....
   double postCircumference = 22.5 * PI;
   double coilCircumference = 49.5 * PI;
-  //double phys = 154.97/70.69;  //22.5/49.33
+  double shuttleCircumference = 184 * PI;
+  
   double phys = coilCircumference/postCircumference;
   
   double fullRotationSteps = (360.0 / (11.25 / gearRatios) * phys);
-  
-  
+    
   enum motion {UP, DOWN, STOP};
   void doStep(motion dir);
   
@@ -50,25 +51,21 @@
 // ============================================================
 // ============================================================
 // ============================================================
-
-/*
- *   Pin D2 = INT0
- *   Pin D3 = INT1
- */
  
   // ------------------------------------------------------
   // iterupt for IR winder loop counter 
   // ------------------------------------------------------
-  const byte interruptPinIR = 2; // pin 2
+  const byte interruptPinIR = 2; // pin 2, Pin D2 = INT0
   volatile unsigned long loopCount = 0;
   
   // ------------------------------------------------------
   // interrupt for rotory encoder
   // ------------------------------------------------------
-  const byte interruptRotoryPin = 3; // pin 3
+  const byte interruptRotoryPin = 3; // pin 3, Pin D3 = INT1
   const byte directionPin = 4;
   volatile byte direction1 = LOW;
-  volatile unsigned long count = 0;
+  volatile int rotoryCount = 0;
+  volatile double tempRotoryCount = 0.0;
   
   const byte rotorySwitch = 13;  // Pin 13
 
@@ -81,42 +78,35 @@
   #include <LiquidCrystal_I2C.h> //from newliquidcrystal library
   LiquidCrystal_I2C lcd(0x27, 20, 2);  // Set the LCD I2C address
 
+void printDouble(double number, byte precision){
+  // Make room for your floating point number, however you plan to format it.
+  char floatBuffer[precision + 1];
+  
+  // Make room for entire print line, however long you plan
+  // to make it.
+  char printBuffer[precision + 1];
 
-
-void printDouble( double val, unsigned int precision){
-// Prints val with number of decimal places determine by precision
-// NOTE: precision is 1 followed by the number of zeros for the desired number of decimial places
-// example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
-
-   Serial.print (int(val));  //prints the int part
-   Serial.print("."); // print the decimal point
-   unsigned int frac;
-   if(val >= 0)
-       frac = (val - int(val)) * precision;
-   else
-       frac = (int(val)- val ) * precision;
-   Serial.println(frac,DEC) ;
+  dtostrf(number, precision, precision, floatBuffer);
+  sprintf(printBuffer, "%s", floatBuffer);
+  Serial.println(printBuffer);
 }
 
-void setup() {
- 
-  // ------------------------------------------------------
-  // open the serial port
-  // ------------------------------------------------------
-  Serial.begin(baudRate);
+void lcdDouble(double number, byte precision){
+  // Make room for your floating point number, however you plan to format it.
+  char floatBuffer[precision + 1];
+  
+  // Make room for entire print line, however long you plan
+  // to make it.
+  char printBuffer[precision + 1];
 
-  Serial.println("Stepper 28BYJ-48 Gear Reduction with ULN2003 driver");
-  Serial.println("=============================");
+  dtostrf(number, precision, precision, floatBuffer);
+  sprintf(printBuffer, "%s mm ", floatBuffer);
 
+  lcd.print(printBuffer);
+}
 
-  Serial.print  ("Gear Ratios: ");
-  Serial.println(gearRatios);
-  Serial.print  ("Full Rotation Steps: ");
-  Serial.println(fullRotationSteps);
-  Serial.print  ("Step size: ");
-  printDouble((360.0/fullRotationSteps),10000);
-
-  //---------------------------------------------------------------
+/* other print double options:
+//---------------------------------------------------------------
   float x = -1.234567890123;
   byte precision = 9;  // Number of digits after the decimal point
   
@@ -147,19 +137,33 @@ void setup() {
   // the whole enchilada in one statement.
   sprintf(printBuffer, "With %%.%df precision, x = %s", precision, floatBuffer);
   Serial.println(printBuffer);
-  
-  
-  dtostrf((360.0/fullRotationSteps), precision+3, precision, floatBuffer);
-  sprintf(printBuffer, "With %%.%df precision, Step size = %s", precision, floatBuffer);
-  Serial.println(printBuffer);
+*/
+
+void setup() {
+ 
+  // ------------------------------------------------------
+  // open the serial port
+  // ------------------------------------------------------
+  Serial.begin(baudRate);
+
+  Serial.println("Stepper 28BYJ-48 Gear Reduction with ULN2003 driver");
+  Serial.println("=============================");
 
 
+  Serial.print  ("Gear Ratios: ");
+  Serial.println(gearRatios);
+  Serial.print  ("Full Rotation Steps: ");
+  Serial.println(fullRotationSteps);
+  Serial.print  ("Step size: ");
+  byte precision = 9;  // Number of digits after the decimal point
+  printDouble(360.0/fullRotationSteps, precision);
+ 
   // ------------------------------------------------------
   // initialize winder gear.
   // ------------------------------------------------------
   pinMode(motorEnable, OUTPUT);
   pinMode(motor1A, OUTPUT);
-  pinMode(motor2a, OUTPUT);
+  pinMode(motor2A, OUTPUT);
       
   // ------------------------------------------------------
   // initialize Stepper motor.
@@ -175,15 +179,14 @@ void setup() {
   pinMode (interruptPinIR, INPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPinIR), pinIRInterrupt, FALLING);
   
-    // ------------------------------------------------------
+  // ------------------------------------------------------
   // interrupt for rotory encoder
   // ------------------------------------------------------
-    pinMode (interruptRotoryPin, INPUT);
-    pinMode (directionPin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(interruptRotoryPin), interruptRotory, RISING);
+  pinMode (interruptRotoryPin, INPUT);
+  pinMode (directionPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(interruptRotoryPin), interruptRotory, RISING);
 
-    pinMode (rotorySwitch, INPUT);
-    
+  pinMode (rotorySwitch, INPUT);  
   
   // ------------------------------------------------------
   // LCD setup
@@ -194,21 +197,28 @@ void setup() {
   lcd.clear();  
   
   lcd.setCursor(0, 0);
-  lcd.print("Coil Winder V0.1");
+  lcd.print("Coil Winder V0.2");
 
   lcd.setCursor(0, 1);
-  //lcd.print("Q-Bytes World");
   lcd.print("Toolbox Aid");
 
   delay(3000);  
+  //         12345678901234567890
+  lcd.setCursor(0, 0);
+  lcd.print("                    ");
+  lcd.setCursor(0, 1);
+  lcd.print("                    ");
+
+//  lcd.setCursor(0, 1);
+//  lcdDouble(360.0/fullRotationSteps, precision);
+//  delay(3000);  
 }
 
 void doDisplay(){
   lcd.init();
   lcd.backlight();
   
-  //delay(2000);  
-  lcd.clear();  
+//  lcd.clear();  
   
   lcd.setCursor(0, 0);
   lcd.print("Coil Winder");
@@ -301,98 +311,243 @@ const byte onOff  = 5;
 volatile byte state = LOW;
 unsigned long loopCnt = 9999999;
 
-volatile boolean activeRun = false;
 long debouncing_time = 100; //Debouncing Time in Milliseconds
 volatile unsigned long last_millis;
-volatile int b=0;
-
-void Interrupt() {
-  b++;
-  Serial.print ("B: ");
-  Serial.println (b);
-}
-
-void cntDetectIR() {
-  state = digitalRead(interruptPinIR);
-  digitalWrite(onOff,state);
-
-  if (state){      
-    Serial.println("Rise: ");
-    return;
-  }else{
-    Serial.println("Fall: ");
-  }
-
-  if (activeRun){
-    return;
-  }
-// 
-//  if (loopCnt >= gear2StepsPerRev){
-//    loopCnt = 0;
-//    activeRun = true;
-//    Serial.print("arT:");   
-//  }    
-
-  Serial.print(state);   
-  Serial.print(":");   
-  Serial.println(activeRun);  
-}
 
 void pinIRInterrupt(){
   loopCount++;
 }
+
 void interruptRotory() {
   int amount = 1;
   
   if((long)(millis() - last_millis) <= debouncing_time) {
     amount = 10;
     }
+    
   last_millis = millis(); //micros();
   
   direction1 = digitalRead(directionPin); 
   if (direction1 == 1){
-    count += amount;
+    if (rotoryCount + amount <= 9999){
+      rotoryCount += amount;
+    }
   }else{
-    count -= amount;
+    if (rotoryCount - amount > 0){
+      rotoryCount -= amount;
+    }
   }
 
-
   Serial.print  ("Interrupt: ");
   Serial.print  (direction1);
-
-  Serial.print  ("Interrupt: ");
-  Serial.print  (direction1);
-  Serial.print  (" cnt: ");
-  Serial.println(count);
+  Serial.print  (" rotoryCount: ");
+  Serial.println(rotoryCount);
 }
 
-//void debounceInterrupt() {
-//  if((long)(millis() - last_millis) >= debouncing_time * 1000) {
-//    Interrupt();
-//    cntDetectIR();
-//    last_millis = millis(); //micros();
-//  }else{
-//    Serial.println ("early");
-//    return;
-//  }
-//
-//  Serial.print ("Debounce Time: ");
-//  Serial.println (debouncing_time * 1000);
-//    
-//  Serial.print ("last_micros Time: ");
-//  Serial.println (last_millis);
-//}
+double toroidOD = 0.0;
+double toroidID = 0.0;
+double toroidH  = 0.0;
+int windings = 0;
+double wireWidth = 0;
+
+
+int debounceDelay = 100;
+void debounceSwitch(){
+  while(!digitalRead(rotorySwitch)){
+    delay(debounceDelay);
+  }
+  delay(debounceDelay * 4);
+}
+
+void setOD(){
+  lcd.setCursor(0, 0);
+  lcd.print("Toroid");
+  lcd.setCursor(0, 1);
+  lcd.print("OD:");
+
+  rotoryCount = 1;
+  rotoryCount = 493;
+  
+  while(digitalRead(rotorySwitch)){
+    tempRotoryCount = (double)rotoryCount / 10.0;
+    if (tempRotoryCount !=  toroidOD){
+      toroidOD = tempRotoryCount;
+      lcd.setCursor(6, 1);
+      lcdDouble(toroidOD, 1);
+    }
+  }
+  debounceSwitch();
+}
+
+void setID(){
+  lcd.setCursor(0, 0);
+  lcd.print("Toroid");
+  lcd.setCursor(0, 1);
+  lcd.print("ID:");
+
+  rotoryCount = 1;
+  rotoryCount = 314;
+  while(digitalRead(rotorySwitch)){
+    tempRotoryCount = (double)rotoryCount / 10.0;
+    if (tempRotoryCount !=  toroidID){
+      toroidID = tempRotoryCount;
+      lcd.setCursor(6, 1);
+      lcdDouble(toroidID, 1);
+    }
+  }
+  debounceSwitch();
+}
+
+void setH(){
+  lcd.setCursor(0, 0);
+  lcd.print("Toroid");
+  lcd.setCursor(0, 1);
+  lcd.print("H:");
+
+  rotoryCount = 1;
+  rotoryCount = 165;
+  while(digitalRead(rotorySwitch)){
+    tempRotoryCount = (double)rotoryCount / 10.0;
+    if (tempRotoryCount !=  toroidH){
+      toroidH = tempRotoryCount;
+      lcd.setCursor(6, 1);
+      lcdDouble(toroidH, 1);
+    }
+  }
+  debounceSwitch();
+}
+
+void setWindings(){
+  lcd.setCursor(0, 0);
+  lcd.print("Toroid");
+  lcd.setCursor(0, 1);
+  lcd.print("Windings: ");
+
+  rotoryCount = 1;
+  rotoryCount = 100;
+  while(digitalRead(rotorySwitch)){
+    if (rotoryCount !=  windings){
+      windings = rotoryCount;
+      lcd.setCursor(10, 1);
+      lcd.print(windings);
+      lcd.print(" ");
+    }
+  }
+  debounceSwitch();
+}
+
+void setWireWidth(){
+  lcd.setCursor(0, 0);
+  lcd.print("Toroid wire");
+  lcd.setCursor(0, 1);
+  lcd.print("Width: ");
+
+  rotoryCount = 1;
+  rotoryCount = 64;
+  while(digitalRead(rotorySwitch)){
+    tempRotoryCount = (double)rotoryCount / 100.0;
+    if (tempRotoryCount !=  wireWidth){
+      wireWidth = tempRotoryCount;
+      lcd.setCursor(8, 1);
+      lcdDouble(wireWidth, 2);
+    }
+  }
+  debounceSwitch();
+}
+
+void loadShuttle(){
+  lcd.setCursor(0, 0);
+  lcd.print("Load Shuttle");
+  lcd.setCursor(0, 1);
+  lcd.print("M/W: ");
+
+  
+  // meters required
+  double toroidW = (toroidOD - toroidID) / 2;
+  Serial.print  ("toroidW: ");
+  Serial.println(toroidW);
+
+  double area = (toroidW + toroidH) * 2;
+  Serial.print  ("area: ");
+  Serial.println(area);
+
+  double milli = (area * windings);
+  Serial.print  ("milli: ");
+  Serial.println(milli);
+
+  Serial.print  ("Shuttle Circum: ");
+  Serial.println(shuttleCircumference);
+
+  double shuttleWindings = milli / shuttleCircumference;
+  Serial.print  ("shuttleWindings: ");
+  Serial.println(shuttleWindings);
+
+  double meters = milli / 1000;
+  Serial.print  ("Meters: ");
+  Serial.println(meters);
+
+  // load loops
+  // number of overlaps
+  lcd.print(meters);
+  lcd.print("/");
+  lcd.print(shuttleWindings);
+
+  Serial.print  ("Shuttle Windings: ");
+  Serial.println(shuttleWindings);
+
+
+
+  while(digitalRead(rotorySwitch)){
+  
+  }
+  debounceSwitch();
+
+  // Load the shuttle
+  // run motor revers
+  // count loops +1
+  // stop motor;
+}
+
+void   loadWindToroid(){
+  lcd.setCursor(0, 0);
+  lcd.print("Wind Coil");
+  lcd.setCursor(0, 1);
+  lcd.print("Loop / ETA : ");
+
+  // load loops
+  // pause for overlaps
+
+  while(digitalRead(rotorySwitch)){
+  
+  }
+  debounceSwitch();
+
+  // Load the shuttle
+  // run motor revers
+  // count loops +1
+  // increment stepper (wire diameter on ID but step for OD to meet the goal)
+  // stop motor;
+}
 
 
 // LOOP function runs over and over again forever
 void loop() {
 
+  setOD();  
+  setID();  
+  setH();  
+  setWindings();
+  setWireWidth();
+  loadShuttle();
+  loadWindToroid();
+  
+  
   doDisplay();
 
   lcd.setCursor(0, 1);
-  lcd.print("Count:       ");
+  lcd.print("rotoryCount:       ");
   lcd.setCursor(6, 1);
-  lcd.print(count);
+  lcd.print(rotoryCount);
       
   lcd.setCursor(10, 1);
   lcd.print(loopCount);
@@ -400,7 +555,7 @@ void loop() {
 
   while (true){
     digitalWrite(motor1A, LOW); 
-    digitalWrite(motor2a, HIGH); 
+    digitalWrite(motor2A, HIGH); 
     digitalWrite(motorEnable, HIGH); 
 //    delay(2000); // waits for a 2 ms to simulate step deley
 while (digitalRead(rotorySwitch));
@@ -409,7 +564,7 @@ while (digitalRead(rotorySwitch));
 
   
     digitalWrite(motor1A, HIGH); 
-    digitalWrite(motor2a, LOW); 
+    digitalWrite(motor2A, LOW); 
     digitalWrite(motorEnable, HIGH); 
 //    delay(2000); // waits for a 2 ms to simulate step deley
 while (digitalRead(rotorySwitch));
